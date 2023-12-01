7c2117486d5bdad03c2bca7e0061c0f6e5218f30
LF305 <122302120+LF305@users.noreply.github.com>
2023-12-01 12:20:48 -0500

    Fixed current and voltage formulas

--- arduino_current_voltage_print/arduino_current_voltage_print.ino
+++ arduino_current_voltage_print/arduino_current_voltage_print.ino
@@ -25,6 +25,7 @@ pinMode(35, OUTPUT);
 pinMode(36, OUTPUT);
 pinMode(37, OUTPUT);
 
+pinMode(A0, INPUT);
 pinMode(A1, INPUT);
 
 Serial.begin(38400);
@@ -36,7 +37,12 @@ void loop() {
   for (int c = 0; c <= 199; c++) {
     switch(roundedCurrent[c]) {
       case 16:
-        digitalWrite(22, HIGH);
+      for (int b = 22; b <=37; b++){
+        digitalWrite(b, HIGH);
+        if (b > 22) {
+          digitalWrite(b, LOW);
+        }
+      }
       break;
 
       case 32:
@@ -72,88 +78,98 @@ void loop() {
         if (b > 26) {
           digitalWrite(b, LOW);
         }
-      break;
       }
+      break;
+      
       case 86:
       for (int b = 23; b <=37; b++){
         digitalWrite(b, HIGH);
         if (b > 27) {
           digitalWrite(b, LOW);
         }
-      break;
       }
+      break;
+      
       case 97:
       for (int b = 23; b <=37; b++){
         digitalWrite(b, HIGH);
         if (b > 28) {
           digitalWrite(b, LOW);
         }
-      break;
       }
+      break;
+      
       case 108:
       for (int b = 23; b <=37; b++){
         digitalWrite(b, HIGH);
         if (b > 29) {
           digitalWrite(b, LOW);
         }
-      break;
       }
+      break;
+
       case 118:
       for (int b = 23; b <=37; b++){
         digitalWrite(b, HIGH);
         if (b > 30) {
           digitalWrite(b, LOW);
         }
-      break;
       }
+      break;
+
       case 127:
       for (int b = 23; b <=37; b++){
         digitalWrite(b, HIGH);
         if (b > 31) {
           digitalWrite(b, LOW);
         }
-      break;
       }
+      break;
+
       case 135:
       for (int b = 23; b <=37; b++){
         digitalWrite(b, HIGH);
         if (b > 32) {
           digitalWrite(b, LOW);
         }
-      break;
       }
+      break;
+
       case 142:
       for (int b = 23; b <=37; b++){
         digitalWrite(b, HIGH);
         if (b > 33) {
           digitalWrite(b, LOW);
         }
-      break;
       }
+      break;
+
       case 149:
       for (int b = 23; b <=37; b++){
         digitalWrite(b, HIGH);
         if (b > 34) {
           digitalWrite(b, LOW);
         }
-      break;
       }
+      break;
+      
       case 155:
       for (int b = 23; b <=37; b++){
         digitalWrite(b, HIGH);
         if (b > 35) {
           digitalWrite(b, LOW);
         }
-      break;
       }
+      break;
+
       case 160:
       for (int b = 23; b <=37; b++){
         digitalWrite(b, HIGH);
         if (b > 36) {
           digitalWrite(b, LOW);
         }
-      break;
       }
+      break;
 
       case 164:
       for (int b = 23; b <=37; b++){
@@ -161,8 +177,8 @@ void loop() {
         if (b > 37) {
           digitalWrite(b, LOW);
         }
-      break;
       }
+      break;
 
       default:
       for (int a = 23; a <=37; a++){
@@ -170,7 +186,7 @@ void loop() {
       }
     }
     delay(1000);
-     VIPrint(analogRead(A1));
+     VIPrint(analogRead(A0), analogRead(A1));
   }
   for (int a = 22; a <=37; a++){
     digitalWrite(a, LOW);
@@ -178,11 +194,12 @@ void loop() {
 }
 
 //Function that prints sensor data to excel. To be called within the loop that sets the loads on? Or would that delay too much?
-void VIPrint(double sensorValue){
+void VIPrint(double voltageValue, double currentValue){
 // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float  voltage = sensorValue*(5.0 / 1023.0);
-  //Gets current based on voltage
- float current = (voltage - 0.6)*50;
+  float  voltage = voltageValue*(5.0 / 1023.0)*6;
+  //Reads current from A1 pin and converts to normal value
+ //float current = (currentValue - 0.6)*50;
+ float current = (currentValue*(5.0 / 1023.0) - 0.59)*50;
 
 //Prints voltage and current to excel
   Serial.print(voltage); 
